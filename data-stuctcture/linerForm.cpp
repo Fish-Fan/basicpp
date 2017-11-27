f82f331814abe1c1b5dbe84fe8f49b9e4cc3ba64
Fish-Fan <fanyank@126.com>
2017-11-27 17:13:20 +0800

    add tree

---
+++ data-stuctcture/linerForm.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+typedef 1 OK;
+typedef 0 ERROR;
+typedef 10 INIT_SIZE;
+
+class  LinerForm {
+  public:
+    int array[INIT_SIZE];
+    int[] init() {
+      return array;
+    }
+
+    
+}
